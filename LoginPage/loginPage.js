21401d77941f0db870044ccd77a046c90297184d
mbicanin99 <mbicanin99f@gmail.com>
2023-09-27 17:33:04 +0200

    Added error p  in loginPage.html, updated validation function in validation.js and adeed code for checking username and password in loginPage.js

--- LoginPage/loginPage.js
+++ LoginPage/loginPage.js
@@ -1,1 +1,25 @@
-'use strict'
+'use strict';
+
+document.getElementById('loginButton').addEventListener('click',function(event){
+    event.preventDefault(); 
+    let username = document.getElementById('username').value;
+    let passwordInput = document.getElementById('password');
+    let passwordValue = passwordInput.value;
+
+   
+    
+    if (username.trim() === '') {
+        const usernameError = document.getElementById('usernameError');
+        usernameError.textContent = 'Username is required!';
+    }
+
+    if(!validation(passwordValue)){
+        const passwordError = document.getElementById('passwordError');
+        passwordError.textContent = 'Password validation failed!';
+        passwordError.style.display = 'block';
+
+    }else{
+        window.location.href = '../Recipe/recipePage.html';
+    }
+}
+    );
--- LoginPage/validation.js
+++ LoginPage/validation.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 function validation(password){
     if(typeof password!=='string'){
@@ -6,36 +6,48 @@ function validation(password){
         return false;
     }
 
+  let isValid=true;
+
    const regexTwoRepetedCharacters= /^(?!.*(.)\1\1)[A-Za-z0-9!\"#$%&'()=*?+@{}[\]:;]*$/;
    const regexAtLeastTwoSpecialCharacters=/(?:[!\"#$%&'()=*?+@{}[\]:;].*){2,}/;
   
     if(password.length<6&&password>24){
         console.log('Password length must be between 6 and 24 characters!');
-    
+        isValid=false;
     }
 
     if (!/[A-Z]/.test(password)) {
         console.log('Password must have at least one uppercase letter!');
-        
+        isValid=false;
       }
 
       if (!/[a-z]/.test(password)) {
         console.log('Password must have at least one lowercase letter!');
-        
+        isValid=false;
       }
 
       if (!/[0-9]/.test(password)) {
         console.log('Password must have at least one digit!');
-
+        isValid=false;
       }
 
       if(!regexTwoRepetedCharacters.test(password)){
         console.log('Password must include maximum two repeted characters!')
+        console.log(password)
+        isValid=false;
       }
 
       if(!regexAtLeastTwoSpecialCharacters.test(password)){
         console.log('Password must have at least two special characters (! “ # $ % & ‘ ( ) = * ? + @ { } [ ] : ;)');
-        
+        isValid=false;
       }
+
+      if(!isValid){
+        console.log('Password validation failed!');
+      }else{
+        console.log('Password is valid!')
+      }
+      
+    return isValid;
     
 }
